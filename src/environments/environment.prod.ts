ed82e89fe4e016e5dc2a0fb3a217fa0abd068432
telekpsi <19208571-telekpsi@users.noreply.github.com>
2022-10-06 10:25:49 -0600

    shouldn't actually change anything

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC8PaRNF-INSUZD6yY5CywyKjYVAWvgswg",
+    authDomain: "clips-fca84.firebaseapp.com",
+    projectId: "clips-fca84",
+    storageBucket: "clips-fca84.appspot.com",
+    appId: "1:806078530886:web:7cfbc98556886779a55147"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC8PaRNF-INSUZD6yY5CywyKjYVAWvgswg",
+    authDomain: "clips-fca84.firebaseapp.com",
+    projectId: "clips-fca84",
+    storageBucket: "clips-fca84.appspot.com",
+    appId: "1:806078530886:web:7cfbc98556886779a55147"
+  }
 };
 
 /*
