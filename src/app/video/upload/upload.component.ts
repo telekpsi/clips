899adf9468361b7db570474d4a66f7ca17a56981
telekpsi <19208571-telekpsi@users.noreply.github.com>
2022-10-07 10:39:52 -0600

    Upload component integrated

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,27 +1,130 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
+import { v4 as uuid } from 'uuid';
+import { last, switchMap } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { ClipService } from 'src/app/services/clip.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnDestroy {
   isDragover = false;
   file: File | null = null;
+  nextStep = false;
+  inSubmission = false;
+  showAlert = false;
+  alertMsg = 'Please wait! Your clip is being uploaded';
+  alertColor = 'blue';
+  percentage = 0;
+  showPercentage = false;
+  user: firebase.User | null = null;
+  task?: AngularFireUploadTask;
 
-  constructor() { }
+  title = new FormControl('', {
+    validators: [
+    Validators.required,
+    Validators.minLength(3)
+    ],
+    nonNullable: true
+  });
+  uploadForm = new FormGroup({
+    title: this.title
+  });
 
-  ngOnInit(): void {
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipsService: ClipService,
+    private router: Router
+  ) { 
+    auth.user.subscribe(user => this.user = user);
+  }
+
+  ngOnDestroy(): void {
+    this.task?.cancel();
   }
 
   storeFile($event: Event) {
     this.isDragover = false;
 
+    this.file = ($event as DragEvent).dataTransfer ?
+      ($event as DragEvent).dataTransfer?.files.item(0) ?? null :
+      ($event.target as HTMLInputElement).files?.item(0) ?? null;
+
     if (!this.file || this.file.type !== 'video/mp4') {
-      
+      this.alertMsg = 'An unexpected error occurred. Please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
     }
 
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    );
+    this.nextStep = true;
+  }
+
+  uploadFile() {
+    this.uploadForm.disable();
+
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your clip is being uploaded';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+    this.showPercentage = true;
+
+    const clipFileName = uuid()
+    const clipPath = `clips/${clipFileName}.mp4`;
+    
+    this.task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
+
+    this.task.percentageChanges().subscribe(progress => {
+      this.percentage = progress as number / 100
+    });
+
+    this.task.snapshotChanges().pipe(
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
+    ).subscribe({
+      next: async (url) => {
+        this.alertColor = 'green'
+        const clip = {
+          uid: this.user?.uid as string,
+          displayName: this.user?.displayName as string,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        };
+
+        const clipDocRef = await this.clipsService.createClip(clip);
+
+        this.alertMsg = 'Success! Your clip is now ready to share with the world.';
+        this.showPercentage = false;
+
+        setTimeout(() => {
+          this.router.navigate([
+            'clip', clipDocRef.id
+          ]);
+        }, 1000);
+      },
+      error: (error) => {
+        this.uploadForm.enable();
+        
+        this.alertColor = 'red';
+        this.alertMsg = 'Upload failed! Please try again later.';
+        this.inSubmission = true;
+        this.showPercentage = false;
+        console.error(error);
+      }
+    });
   }
 
 }
